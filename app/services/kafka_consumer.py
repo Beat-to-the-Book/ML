ab127ab248f479b207d33cae989aea8adecb8327
Yugyeong Park <bonbonly7@naver.com>
2025-03-23 16:23:00 +0900

    feature: Kafka를 통해 추천 요청 메시지를 수신

---
+++ app/services/kafka_consumer.py
@@ -0,0 +1,19 @@
+from kafka import KafkaConsumer
+import json
+from app.services.recommend_service import get_similar_books
+from app.services.recommend_cache_service import cache_recommendations
+from app.config import Config
+
+consumer = KafkaConsumer(
+    'recommendation_topic',
+    bootstrap_servers=[Config.KAFKA_SERVER],
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
+
+for message in consumer:
+    data = message.value
+    user_id = data['userId']
+    title, author, genre = data['title'], data['author'], data['genre']
+
+    recommended_books = get_similar_books(title, author, genre)
+    cache_recommendations(user_id, recommended_books)
