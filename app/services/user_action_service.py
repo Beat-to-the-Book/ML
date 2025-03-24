cb10aeea34f81a992f7926ba2c8b554e0e167f74
Yugyeong Park <bonbonly7@naver.com>
2025-03-24 23:31:26 +0900

    Bug: Config 경로 관련 에러 수정

--- app/services/user_action_service.py
+++ app/services/user_action_service.py
@@ -1,6 +1,6 @@
 import redis
 import json
-from config import Config
+from app.config import Config
 
 redis_client = redis.Redis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, decode_responses=True)
 
