c41643c3f5a726637f47896678e7441d23227ad9
Yugyeong Park <bonbonly7@naver.com>
2025-03-24 23:31:06 +0900

    Bug: 데이터베이스 관련 에러 수정

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from app.routes.recommend_routes import recommend_bp
 from app.config import Config
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    db.init_app(app)
 
     @app.route("/")
     def root_health():
--- app/config.py
+++ app/config.py
@@ -4,3 +4,6 @@ class Config:
     REDIS_HOST = "redis"
     REDIS_PORT = 6379
     CACHE_EXPIRE = 600  # 10분
+
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:password@beat_to_the_book_mysql:3306/beat_to_the_book"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # 객체 추적 X (성능 최적화)
