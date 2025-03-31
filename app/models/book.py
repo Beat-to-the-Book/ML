c4273106215fe30e407a16de1add4d1d961ec8da
Yugyeong Park <bonbonly7@naver.com>
2025-03-31 20:01:15 +0900

    fix: Book 테이블 수정

--- app/models/book.py
+++ app/models/book.py
@@ -5,15 +5,23 @@ class Book(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(255), nullable=False)
-    author = db.Column(db.String(255), nullable=False)
-    genre = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(50), nullable=False)
+    bookDescription = db.Column(db.String(2000))
     price = db.Column(db.Float, nullable=False)
+    publisher = db.Column(db.String(100))
+    publishDate = db.Column(db.String(255))
+    coverImageUrl = db.Column(db.String(500))
 
-    def __init__(self, title, author, genre, price):
+    def __init__(self, title, author, genre, bookDescription, price, publisher, publishDate, coverImageUrl):
         self.title = title
         self.author = author
         self.genre = genre
+        self.bookDescription = bookDescription
         self.price = price
+        self.publisher = publisher
+        self.publishDate = publishDate
+        self.coverImageUrl = coverImageUrl
 
     def to_dict(self):
         """ JSON 직렬화용 딕셔너리 변환 함수 """
@@ -22,5 +30,9 @@ class Book(db.Model):
             "title": self.title,
             "author": self.author,
             "genre": self.genre,
-            "price": self.price
+            "bookDescription": self.bookDescription,
+            "price": self.price,
+            "publisher": self.publisher,
+            "publishDate": self.publishDate,
+            "coverImageUrl": self.coverImageUrl,
         }
