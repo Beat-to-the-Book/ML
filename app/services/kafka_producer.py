108309a091a89ab1cf8ee092b1ae2a750cdf39da
Yugyeong Park <bonbonly7@naver.com>
2025-04-26 21:13:55 +0900

    fix: kafka_producer - 메시지 송신 측 로그 변경

--- app/services/kafka_producer.py
+++ app/services/kafka_producer.py
@@ -5,8 +5,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-# Flask -> Spring
-# Kafka Producer: Flask에서 추천 도서를 다시 Spring으로 전달
+# Flask → Spring으로 보내는 Kafka Producer
 producer = KafkaProducer(
     bootstrap_servers=[Config.KAFKA_SERVER],
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
@@ -20,6 +19,6 @@ def send_recommendations_to_kafka(user_id, recommendations):
 
     try:
         producer.send("recommendation_topic", message)
-        logger.info(f"Kafka 전송 완료: userId={user_id}, books={len(recommendations)}권")
+        logger.info(f"Kafka 전송 완료: userId={user_id}, 추천 책 수={len(recommendations)}권")
     except Exception as e:
         logger.error(f"Kafka 전송 실패: {e}")
