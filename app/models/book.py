8aa5740835a6267f873b86ef23772a706c6b69ee
Yugyeong Park <bonbonly7@naver.com>
2025-04-26 21:11:33 +0900

    fix: Book DB 변경

--- app/models/book.py
+++ app/models/book.py
@@ -6,22 +6,25 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(255), nullable=False)
     author = db.Column(db.String(100), nullable=False)
-    genre = db.Column(db.String(50), nullable=False)
-    bookDescription = db.Column(db.String(2000))
+    genre = db.Column(db.String(500), nullable=False)
     price = db.Column(db.Float, nullable=False)
     publisher = db.Column(db.String(100))
-    publishDate = db.Column(db.String(255))
-    coverImageUrl = db.Column(db.String(500))
+    publishDate = db.Column('publish_date', db.String(255))
+    leftCoverImageUrl = db.Column('left_cover_image_url', db.String(500))
+    frontCoverImageUrl = db.Column('front_cover_image_url', db.String(500))
+    backCoverImageUrl = db.Column('back_cover_image_url', db.String(500))
 
-    def __init__(self, title, author, genre, bookDescription, price, publisher, publishDate, coverImageUrl):
+    def __init__(self, title, author, genre, price, publisher=None, publishDate=None,
+                 leftCoverImageUrl=None, frontCoverImageUrl=None, backCoverImageUrl=None):
         self.title = title
         self.author = author
         self.genre = genre
-        self.bookDescription = bookDescription
         self.price = price
         self.publisher = publisher
         self.publishDate = publishDate
-        self.coverImageUrl = coverImageUrl
+        self.leftCoverImageUrl = leftCoverImageUrl
+        self.frontCoverImageUrl = frontCoverImageUrl
+        self.backCoverImageUrl = backCoverImageUrl
 
     def to_dict(self):
         """ JSON 직렬화용 딕셔너리 변환 함수 """
@@ -30,9 +33,10 @@ class Book(db.Model):
             "title": self.title,
             "author": self.author,
             "genre": self.genre,
-            "bookDescription": self.bookDescription,
             "price": self.price,
             "publisher": self.publisher,
             "publishDate": self.publishDate,
-            "coverImageUrl": self.coverImageUrl,
+            "leftCoverImageUrl": self.leftCoverImageUrl,
+            "frontCoverImageUrl": self.frontCoverImageUrl,
+            "backCoverImageUrl": self.backCoverImageUrl,
         }
