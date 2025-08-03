a1551d29088563015a964de78c16eccce08a42ee
Yugyeong Park <bonbonly7@naver.com>
2025-08-03 17:22:42 +0900

    fix: 버퍼에 남아 있는 모든 메시지를 브로커에 즉시 전송하여 프로그램 종료 직전에 모든 메시지를 보장

--- app/infra/kafka/kafka_producer.py
+++ app/infra/kafka/kafka_producer.py
@@ -19,6 +19,7 @@ def send_recommendations_to_kafka(user_id, recommendations):
 
     try:
         producer.send("recommendation_topic", message)
+        producer.flush() # 버퍼에 남아 있는 모든 메시지를 브로커에 즉시 전송 (동기)
         logger.info(f"Kafka 전송 완료: userId={user_id}, 추천 책 수={len(recommendations)}권")
     except Exception as e:
         logger.error(f"Kafka 전송 실패: {e}")
