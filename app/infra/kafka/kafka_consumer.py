39768c6d03d25faa363cf91d711ab19827e366f6
Yugyeong Park <bonbonly7@naver.com>
2025-08-03 17:28:29 +0900

    refactor: flask의 안정성을 보장하기 위해 flask 웹 서버와 kafka_consumer를 분리

--- app/infra/kafka/kafka_consumer.py
+++ app/infra/kafka/kafka_consumer.py
@@ -5,60 +5,68 @@ from app.core.dependencies import db_session, redis_client
 from app.models.book import Book
 from app.repositories.book_repository import BookPoolRepository
 from app.services.hybrid_recommender import HybridRecommender
-from app.services.recommend_cache import cache_recommendations
 from app.infra.kafka.kafka_producer import send_recommendations_to_kafka
 from app.core.config import Config
 from app.core.logger import setup_logger
+from time import time
 
 logger = setup_logger()
 app = create_app()
 
-# Kafka Consumer 설정
-consumer = KafkaConsumer(
-    'flask_recommendation_topic',
-    bootstrap_servers=[Config.KAFKA_SERVER],
-    value_deserializer=lambda x: json.loads(x.decode('utf-8')),
-    auto_offset_reset='earliest',
-    group_id='flask-consumer-group'
-)
+def main():
+    # Kafka Consumer 설정
+    consumer = KafkaConsumer(
+        'flask_recommendation_topic',
+        bootstrap_servers=[Config.KAFKA_SERVER],
+        value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+        auto_offset_reset='earliest',
+        group_id='flask-consumer-group'
+    )
 
-# Flask 앱 컨텍스트 안에서 실행
-with app.app_context():
-    # 객체 초기화
-    book_repository = BookPoolRepository(db_session, Book, redis_client)
-    recommender = HybridRecommender(book_repository)
+    # Flask 앱 컨텍스트 안에서 실행
+    with app.app_context():
+        # 객체 초기화
+        book_repository = BookPoolRepository(db_session, Book, redis_client)
+        recommender = HybridRecommender(book_repository)
 
-    logger.info("[KafkaConsumer] 추천 시스템 준비 완료")
+        logger.info("[KafkaConsumer] 추천 시스템 준비 완료")
 
-    for message in consumer:
-        try:
-            data = message.value if isinstance(message.value, dict) else json.loads(message.value)
+        for message in consumer:
+            handle(message.value, recommender)
 
-            user_id = data.get('userId')
-            read_books = data.get('readBooks', [])
-            behavior = data.get('userBehaviors', [])
+def handle(message, recommender):
+    try:
+        # 메시지 값 json 파싱
+        data = json.loads(message)
 
-            logger.info(f"[Kafka] 수신 메시지 - userId={user_id}, 읽은 책 수={len(read_books)}, 행동 수={len(behavior)}")
+        user_id = data.get('userId')
+        if not user_id:
+            logger.warning("Kafka 메시지에 userId 없음");
+            return
 
-            if not user_id:
-                logger.warning("Kafka 메시지에 userId 없음")
-                continue
+        read_books = data.get('readBooks', [])
+        behavior = data.get('userBehaviors', [])
+        logger.info(f"[Kafka] 수신 메시지 - userId={user_id}, 읽은 책 수={len(read_books)}, 행동 수={len(behavior)}")
 
-            if not isinstance(read_books, list) or not isinstance(behavior, list):
-                logger.warning("Kafka 메시지 형식 오류")
-                continue
+        # 추천 연산
+        start_time = time()
+        recommendations = recommender.get_hybrid_recommendations(read_books, behavior)
+        logger.info(f"[Kafka] 추천 연산 소요 시간: {round(time() - start_time, 2)}초")
 
-            recommendations = recommender.get_recommendations(read_books, behavior)
+        if recommendations:
+            logger.info(f"[Kafka] 추천 완료 - 추천 수={len(recommendations)}")
+            send_recommendations_to_kafka(user_id, recommendations)
+        else:
+            logger.warning(f"[Kafka] 추천 결과 없음 - userId={user_id}")
 
-            if recommendations:
-                logger.info(f"[Kafka] 추천 완료 - 추천 수={len(recommendations)}")
-                cache_recommendations(user_id, {
-                    "userId": user_id,
-                    "recommendedBooks": recommendations
-                })
-                send_recommendations_to_kafka(user_id, recommendations)
-            else:
-                logger.warning(f"[Kafka] 추천 결과 없음 - userId={user_id}")
+    except Exception as e:
+        logger.error(f"[Kafka] 메시지 처리 중 예외 발생: {e}", exc_info=True)
 
-        except Exception as e:
-            logger.error(f"[Kafka] 메시지 처리 중 예외 발생: {e}", exc_info=True)
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logger.exception(e)
+        # 컨테이너가 꺼지지 않도록 웹 서비스와 kafka consumer 분리
+        while True:
+            time.sleep(60)
