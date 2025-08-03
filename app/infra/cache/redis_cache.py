800b91331001f65a063ef64692ee4cabec7c5a36
Yugyeong Park <bonbonly7@naver.com>
2025-08-03 17:20:50 +0900

    fix: 캐시 유효 기간(TTL)의 디폴트 값을 600s로 설정

--- app/infra/cache/redis_cache.py
+++ app/infra/cache/redis_cache.py
@@ -1,6 +1,6 @@
 import json
 
-def set_cache(redis_client, key, value, expiration):
+def set_cache(redis_client, key, value, expiration=600): # 캐시 유효 기간(TTL): 600s(10m)
     redis_client.set(key, json.dumps(value), ex=expiration)
 
 def get_cache(redis_client, key):
